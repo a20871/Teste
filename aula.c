a6fcdfb46f0b8bc16de433bceb02c3c1dfdff64e
smartins <a20871@alunos.ipca.pt>
2020-10-26 22:40:21 +0000

    very good. Comentário

--- aula.c
+++ aula.c
@@ -1,19 +1,6 @@
-/*****************************************************************//**
- * \file   aula.c
- * \brief  Primeiro programa em C
- * Neste programa o obrejtivo é criar um executável e documentá-lo
- * 
- * \author Sérgio
- * \@e-mail a20871@alunos.ipca.pt
- * 
- * \date   October 2020
- *********************************************************************/
-
 #include <stdio.h>
-int main()
-{
-	int x = 1;					///Atribui uma variável
-	printf("%d\n", x);			///Mostra no ecrã
-	int i = 10;
+int main() {
+	int x = 1;
+	printf("%d\n", x);
 
 }
