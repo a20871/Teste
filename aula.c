b709b1719f6709ee1f07a3e28ef79ba7b9d86b0c
smartins <a20871@alunos.ipca.pt>
2020-10-30 10:53:59 +0000

    Adição de comentário

--- aula.c
+++ aula.c
@@ -1,6 +1,12 @@
-#include <stdio.h>
+/*****************************************************************//**
+ * \file   aula.c
+ * \brief  Utilização de Git
+ * 
+ * \author Sérgio
+ * \date   October 2020
+ *********************************************************************/
 int main() {
-	int x = 1;
+	int x = 1;			/// Utilização da variável x
 	printf("%d\n", x);
 
 }
